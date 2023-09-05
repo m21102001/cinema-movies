d74ce5cb4712b72b916bdffff958b28500d80401
m21102001 <mohammedtarek21102001@gmail.com>
2023-09-05 18:02:09 +0300

    update
    update

--- src/pages/Report/index.jsx
+++ src/pages/Report/index.jsx
@@ -1,9 +1,36 @@
+import React, { useEffect, useState ,useRef} from 'react';
 import styles from './Report.module.scss'
 import { MdFilterListAlt } from 'react-icons/md';
 import { BiCalendar } from 'react-icons/bi'
+import axios from 'axios';
+import {useReactToPrint} from 'react-to-print'
+const baseURL = "https://miracle49-001-site1.itempurl.com/api/testing/reports/tickets?SystemDate=2023-09-03";
+
 const index = () => {
+ const componentRef =useRef()
+ const [post, setPost] = useState([]);
+
+ const handelPrint =useReactToPrint({
+  content:()=>componentRef.current,
+  documentTitle:'emp-data',
+  onAfterPrint:()=> alert('print success'),
+
+
+ })
+  useEffect(() => {
+    axios
+      .get(baseURL)
+      .then((result) => {
+        console.log(result.data);
+        setPost(result.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (!post) return null;
   return (
-    <div className={`shadow p-3 mb-5 bg-body rounded ${styles.root}`}>
+
+    <div ref={componentRef} className={`shadow p-3 mb-5 bg-body rounded ${styles.root}`}>
       <div className='d-flex justify-content-between mx-3 fs-4 fw-semibold'>
         <p >Reports</p>
         <MdFilterListAlt />
@@ -13,30 +40,36 @@ const index = () => {
         <h4 className='mx-3'>Today Date</h4> <BiCalendar />
       </div>
       <h5 className='my-3 ms-4 text-start '>Today Top Movies</h5>
-      <div className="row">
-        <div className="col">
-          <p className='fw-semibold'>Movie Name</p>
-          <span>Spider-Man</span>
-        </div>
-        <div className="col">
-          <p className='fw-semibold'>Sold Tickets</p>
-          <span>560</span>
-        </div>
-        <div className="col">
-          <p className='fw-semibold'>Returned Tickets</p>
-          <span>55</span>
-        </div>
-        <div className="col">
-          <p className='fw-semibold'>Total Tickets</p>
-          <span>505</span>
-        </div>
-        <div className="col">
-          <p className='fw-semibold'>Total Sales</p>
-          <span>$5555</span>
-        </div>
-      </div>
+
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Movie Name</th>
+            <th scope="col">Sold Tickets</th>
+            <th scope="col">Returned Tickets</th>
+            <th scope="col">Total Tickets</th>
+            <th scope="col">Total Sales</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {post?.map((data, index) => { */}
+          {/* return ( */}
+          {/* <tr key={index}> */}
+          <tr >
+            <th scope="row">{post.title}</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>{post.totalPriceForAll}</td>
+          </tr>
+          {/* ); */}
+          {/* })} */}
+        </tbody>
+      </table>
+      <button onClick={handelPrint} type="button" className="btn btn-outline-primary">Print</button>
     </div>
   )
 }
 
 export default index
+
